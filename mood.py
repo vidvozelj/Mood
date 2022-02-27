0e7c449a65b536497011524aa9d6dc4c3ed522b5
vidvozelj <vid.vozelj36@gmail.com>
2022-02-27 11:20:52 +0100

    first commit

---
+++ mood.py
@@ -0,0 +1,14 @@
+mood = input('what is your mood? ')
+
+if mood == 'happy':
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == 'sad':
+    print('Than stop being sad and be awesome instead')
+elif mood == 'excited':
+    print('what for??')
+elif mood == 'relaxed':
+    print('than just chill')
+else:
+    print("I don't recognize this mood")
